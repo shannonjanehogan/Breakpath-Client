aa2dc0b81607d272d4a7fc80de11b0eefd8baaa8
Yashwant Parmar <yashwant.parmar@gmail.com>
2017-07-31 17:53:05 -0700

    App.js updated with screens
    package.json updated with app name and icon
    App.js updated with Log In, Sign Up, and Sign Up to Debate pages
    App.js still needs a navigator
    App.js still needs headers
    App.js still needs a home page and room assignment page

--- App.js
+++ App.js
@@ -1,7 +1,102 @@
 import React from 'react';
-import { AppRegistry, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, AppRegistry, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Picker from 'react-native-picker';
 
-export default class App extends React.Component {
+// export default class LogIn extends React.Component {
+//   // static navigationOptions = {
+//   //   title: 'Sign Up',
+//   // };
+//   constructor(props) {
+//     super(props);
+//     this.state = {text: ''};
+//   }
+//
+//   render() {
+//     return (
+//       <View>
+//         <View style={{padding: 10}}>
+//           <TextInput
+//             style={{height: 40}}
+//             placeholder="Username"
+//           />
+//           <Text style={{padding: 10, fontSize: 42}}>
+//           </Text>
+//         </View>
+//         <View style={{padding: 10}}>
+//           <TextInput
+//             style={{height: 40}}
+//             placeholder="Password"
+//           />
+//           <Text style={{padding: 10, fontSize: 42}}>
+//           </Text>
+//         </View>
+//         <Button
+//           onPress={() => { Alert.alert('Account Created!')}}
+//           title="LOG IN"
+//         />8
+//       </View>
+//     );
+//   }
+// }
+
+// export default class SignUp extends React.Component {
+//   // static navigationOptions = {
+//   //   title: 'Sign Up',
+//   // };
+//   constructor(props) {
+//     super(props);
+//     this.state = {text: ''};
+//   }
+//
+//   render() {
+//     return (
+//       <View>
+//         <View style={{padding: 10}}>
+//           <TextInput
+//             style={{height: 40}}
+//             placeholder="First Name"
+//           />
+//           <Text style={{padding: 10, fontSize: 42}}>
+//           </Text>
+//         </View>
+//         <View style={{padding: 10}}>
+//           <TextInput
+//             style={{height: 40}}
+//             placeholder="Last Name"
+//           />
+//           <Text style={{padding: 10, fontSize: 42}}>
+//           </Text>
+//         </View>
+//         <View style={{padding: 10}}>
+//           <TextInput
+//             style={{height: 40}}
+//             placeholder="Email"
+//           />
+//           <Text style={{padding: 10, fontSize: 42}}>
+//           </Text>
+//         </View>
+//         <View style={{padding: 10}}>
+//           <TextInput
+//             style={{height: 40}}
+//             placeholder="Password"
+//           />
+//           <Text style={{padding: 10, fontSize: 42}}>
+//           </Text>
+//         </View>
+//         <Button
+//           onPress={() => { Alert.alert('Account Created!')}}
+//           title="SIGN UP"
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+export default class DebateSignUp extends React.Component {
+  // static navigationOptions = {
+  //   title: 'Sign Up',
+  // };
   constructor(props) {
     super(props);
     this.state = {text: ''};
@@ -29,23 +124,26 @@ export default class App extends React.Component {
         <View style={{padding: 10}}>
           <TextInput
             style={{height: 40}}
-            placeholder="Email"
+            placeholder="Partner Preference"
           />
           <Text style={{padding: 10, fontSize: 42}}>
           </Text>
         </View>
-        <View style={{padding: 10}}>
-          <TextInput
-            style={{height: 40}}
-            placeholder="Passoword"
-          />
-          <Text style={{padding: 10, fontSize: 42}}>
-          </Text>
-        </View>
+        <Button
+          onPress={() => { Alert.alert('Account Created!')}}
+          title="SIGN UP TO DEBATE"
+        />
       </View>
     );
   }
 }
+
+// const App = StackNavigator({
+//   SignUp: { screen: SignUp },
+//   // LogIn: { screen: LogIn },
+// });
+
+// AppRegistry.registerComponent('App', () => App);
 
 const styles = StyleSheet.create({
   container: {
