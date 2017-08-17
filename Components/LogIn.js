4069ee665aee557cb2d888f7b804b792e4bedf8f
sjhogan2 <shannon.jane.hogan@gmail.com>
2017-08-16 21:48:16 -0700

    Add fetch call and state

--- Components/LogIn.js
+++ Components/LogIn.js
@@ -4,6 +4,33 @@ import { StackNavigator } from 'react-navigation';
 import { Dropdown } from 'react-native-material-dropdown';
 
 export default class LogIn extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+  }
+
+handleLoginClick(event) {
+  event.preventDefault();
+  return fetch('https://breakpath-api.herokuapp.com/login', {
+    method: 'POST',
+    mode: 'no-cors',
+    body: {
+      email: this.state.email,
+      password: this.state.password,
+    },
+  })
+  .then((response) => response.json())
+  .then((responseJson) => {
+    return responseJson.movies;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+}
   static navigationOptions = {
     drawer: () => ({
       title: 'Log In',
